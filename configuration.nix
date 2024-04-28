84c17c6f322073fa7372dc211ad0669099fa25f2
Square-face <linus.michelsson@gmail.com>
2024-04-28 14:57:37 +0200

    Added ZerotierOne service

--- configuration.nix
+++ configuration.nix
@@ -171,6 +171,7 @@
     
     # Enable the OpenSSH daemon.
     services.openssh.enable = true;
+    services.zerotierone.enable = true;
     
     # Open ports in the firewall.
     networking.firewall={
