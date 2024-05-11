10fa4042821df7fe1814158b98b9354f8b225e0f
Square-face <linus.michelsson@gmail.com>
2024-05-11 13:19:11 +0200

    Removed kitty terminal bell sound

--- home.nix
+++ home.nix
@@ -227,6 +227,7 @@
                 font_family = "Fira Code Nerd Font";
                 background_opacity = "0.8";
                 background_blur = "10";
+                enable_audio_bell = "no";
             };
         };
 
